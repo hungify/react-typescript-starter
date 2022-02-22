ee10ff852dc05e39acd4c5286fe9791d474460c7
hungpurdie <nmhung2022@gmail.com>
2022-02-22 18:38:24 +0700

    optimize dependency, use SVG icon by component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
-
 import React from 'react';
 import Counter from '~/components/Counter/Counter';
+import { ReactComponent as Logo } from './logo.svg';
 
 function App() {
   const [count, setCount] = React.useState(0);
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Logo width={'100%'} height={'40vmin'} />
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
 });
