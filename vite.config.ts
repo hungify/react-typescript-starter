a60d26964ff54fda671dbbdb48110bca3883b2aa
hungpurdie <nmhung2022@gmail.com>
2022-03-20 21:36:12 +0700

    add react refresh to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
   plugins: [
@@ -12,5 +13,6 @@ export default defineConfig({
         icon: true,
       },
     }),
+    reactRefresh(),
   ],
 });
