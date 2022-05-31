be79a5d57acbcecb92e3627d650cd1e7887c0903
unknown <nmhung2022@gmail.com>
2022-05-31 16:18:14 +0700

    fix(hot-reload): new config to vite refresh app

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,22 @@ import { useState } from 'react';
 
 import logo from './logo.svg';
 import Counter from '~/components/Counter';
-import Login from '~/pages/Login';
-import Register from '~/pages/Register';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type='button' onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
         </p>
       </header>
       <Counter count={count} />
-      <Login />
-      <Register />
     </div>
   );
 }
