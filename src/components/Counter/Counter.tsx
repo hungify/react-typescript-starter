ae1fafd3a998460a5fcc93e31eb13e510ba7f4fe
hungify <nmhung.dev@gmail.com>
2022-09-28 09:40:53 +0700

    refactor: add new eslint rules

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,8 +1,8 @@
-interface CounterProps {
+interface ICounterProps {
   count: number;
 }
 
-function Counter({ count }: CounterProps) {
+function Counter({ count }: ICounterProps) {
   return (
     <div>
       <h1>Counter {count}</h1>
